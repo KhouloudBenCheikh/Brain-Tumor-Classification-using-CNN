8ca08bd13642e7cf260f732628f28ff4eb18c5d4
Khouloud BenCheikh <khouloudbencheikh011@gmail.com>
2023-02-04 18:20:20 +0100

    Add: Add Training model

--- Train_Model.py
+++ Train_Model.py
@@ -1,16 +1,19 @@
 import cv2
 import os 
-import numpy as np
-from PIL import Image
-from keras.utils import normalize
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D 
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
 import tensorflow as tf
 from tensorflow import keras
 
-images_directory='datasets/'
+from PIL import Image
+import numpy as np
+
+from keras.utils import normalize
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D 
+from keras.layers import Activation, Dropout, Flatten, Dense
+from sklearn.model_selection import train_test_split
+
+
+images_directory='Training_Dataset/'
 #List the content of the Dataset
 no_tumor_images = os.listdir (images_directory + 'no/')
 yes_tumor_images = os.listdir (images_directory + 'yes/')
@@ -65,3 +68,19 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Conv2D(64,(3,3), input_shape=(64,64,3)))
 model.add(Activation ('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
+
+
+#Flatten
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+#Dense=1 because it's a Classification Problem (Tumor/Non Tumor)
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+model.compile(loss='binary_cossentropy', optimizer='adam', metrics= ['accuracy'])
+model.fit(x_train, y_train, batch_size=16, verbose=1, epochs=10, validation_data=(x_test, y_test), shuffle=False)
+
+model.save('BrainTumorClassification10Epochs.h4')
